0428956a7e93f2593f16e33bc93c201a36ceab79
Jonathan Ingersoll <jonathan.ingersoll@gmail.com>
2020-05-18 16:34:24 -0400

    Built Customer Data class; Built Interaction Data class

--- SimpleCRM.Data/Customer.cs
+++ SimpleCRM.Data/Customer.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace SimpleCRM.Data
 {
+    public enum Status { New=1, Engaged, Donor, FollowUp, Uninterested}
+    public enum Role { Owner=1, Manager, Employee, Individual }
     public class Customer
     {
-
+        public int CustomerId { get; set; }
+        public string CustFirstName { get; set; }
+        public string CustLastName { get; set; }
+        public int Organization { get; set; }
+        public int Points { get; set; }
+        public Status Status { get; set; }
+        public int MyProperty { get; set; }
     }
 }
--- SimpleCRM.Data/Interaction.cs
+++ SimpleCRM.Data/Interaction.cs
@@ -8,6 +8,10 @@ namespace SimpleCRM.Data
 {
     public class Interaction
     {
-
+        public int InteractionId { get; set; }
+        public Customer Customer { get; set; }
+        public Event Event { get; set; }
+        public int InteractionPoints { get; set; }
+        public string InteractionNotes { get; set; }
     }
 }
