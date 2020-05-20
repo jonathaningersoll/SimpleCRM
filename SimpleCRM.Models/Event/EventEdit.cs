e2a43491c63c976be00e3e9568e7f5a4d22c1b81
Jonathan Ingersoll <jonathan.ingersoll@gmail.com>
2020-05-20 11:29:05 -0400

    Created Event Detail, Edit, and Create models

--- SimpleCRM.Models/Event/EventEdit.cs
+++ SimpleCRM.Models/Event/EventEdit.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SimpleCRM.Models.Event
 {
-    class EventCreate
+    public class EventEdit
     {
+        public int EventId { get; set; }
+        public DateTimeOffset EventStartTime { get; set; }
+        public DateTimeOffset EventEndTime { get; set; }
+
+        [Display(Name = "Event Title")]
+        public string EventName { get; set; }
+
+        [Display(Name = "Topic")]
+        public string EventTopic { get; set; }
     }
 }
