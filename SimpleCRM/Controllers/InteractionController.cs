0ebbac584979d45f5b8b86c45756c97156df2386
Jonathan Ingersoll <jonathan.ingersoll@gmail.com>
2020-05-26 13:10:03 -0400

    Created InteractionController

---
+++ SimpleCRM/Controllers/InteractionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleCRM.Controllers
+{
+    public class InteractionController : Controller
+    {
+        // GET: Interaction
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
