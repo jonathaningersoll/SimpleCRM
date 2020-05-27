42689d86ee182f08a8a5a8f5d1c641b59114bbc4
Jonathan Ingersoll <jonathan.ingersoll@gmail.com>
2020-05-27 14:58:37 -0400

    Created EventCreate view

--- SimpleCRM.Models/EventModel/EventCreate.cs
+++ SimpleCRM.Models/EventModel/EventCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace SimpleCRM.Models.EventModel
 {
     public class EventCreate
     {
+        [Display(Name ="00/00/0000 00:00:00")]
         public DateTimeOffset EventStartTime { get; set; }
+        [Display(Name = "00/00/0000 00:00:00")]
         public DateTimeOffset EventEndTime { get; set; }
         public string EventName { get; set; }
         public string EventTopic { get; set; }
