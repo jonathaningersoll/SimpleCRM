9b17258320a99a9e63e08dfc0fe68a04aa82abce
Jonathan Ingersoll <jonathan.ingersoll@gmail.com>
2020-05-29 10:51:02 -0400

    Added annotations to OrganizationCreate

--- SimpleCRM.Models/OrganizationModel/OrganizationCreate.cs
+++ SimpleCRM.Models/OrganizationModel/OrganizationCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,13 @@ namespace SimpleCRM.Models.OrganizationModel
 {
     public class OrganizationCreate
     {
+        [Display(Name = "Organization")]
         public string OrganizationName { get; set; }
+        
+        [Display(Name = "Location")]
         public string OrganizationAddress { get; set; }
+
+        [Display(Name = "Industry")]
         public string OrganizationIndustry { get; set; }
     }
 }
