1136de5d652cc7c959c02782a0988e09168e86c5
Jonathan Ingersoll <jonathan.ingersoll@gmail.com>
2020-05-29 11:37:58 -0400

    Added annotations to InteractionEdit

--- SimpleCRM.Models/InteractionModel/InteractionEdit.cs
+++ SimpleCRM.Models/InteractionModel/InteractionEdit.cs
@@ -1,6 +1,7 @@
 ﻿using SimpleCRM.Data;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +13,19 @@ namespace SimpleCRM.Models.InteractionModel
         public int InteractionId { get; set; }
 
         public int CustomerId { get; set; }
+
+        [Display(Name = "Customer:")]
         public string CustomerFullName { get; set; }
 
         public int EventId { get; set; }
+
+        [Display(Name = "Event:")]
         public string EventName { get; set; }
 
+        [Display(Name = "Points to Award:")]
         public int InteractionPointValue { get; set; }
+
+        [Display(Name = "Interaction Notes:")]
         public string InteractionNotes { get; set; }
     }
 }
