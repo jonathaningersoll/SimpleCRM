c04b787a8d08bd825189fbffcce99d4e4844591e
Jonathan Ingersoll <jonathan.ingersoll@gmail.com>
2020-05-29 11:36:25 -0400

    Added annotations to InteractionDetail

--- SimpleCRM.Models/InteractionModel/InteractionDetail.cs
+++ SimpleCRM.Models/InteractionModel/InteractionDetail.cs
@@ -1,6 +1,7 @@
 ﻿using SimpleCRM.Data;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,25 @@ namespace SimpleCRM.Models.InteractionModel
         public int InteractionId { get; set; }
 
         public int CustomerId { get; set; }
+
+        [Display(Name = "Customer:")]
         public string CustomerFullName { get; set; }
 
         public int EventId { get; set; }
+
+        [Display(Name = "Event:")]
         public string Event { get; set; }
 
+        [Display(Name = "Points to Award:")]
         public int InteractionPointValue { get; set; }
+
+        [Display(Name = "Interaction Notes:")]
         public string InteractionNotes { get; set; }
+
+        [Display(Name = "Created:")]
         public DateTimeOffset CreatedUtc { get; set; }
+
+        [Display(Name = "Modified:")]
         public DateTimeOffset ModifiedUtc { get; set; }
     }
 }
